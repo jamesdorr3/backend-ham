974a5c399b3e88e9a17c63888e1cf086aa179ed0
James Dorr <jamesdorr3@gmail.com>
2019-05-15 15:08:39 -0400

    Food.find_or_create_by in backend

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 require 'rest-client'
+require 'json'
+
 class SearchController < ApplicationController
 
   def search
@@ -13,8 +15,31 @@ class SearchController < ApplicationController
     url = "https://trackapi.nutritionix.com/v2"
 
     resp = RestClient.post("#{url}/natural/nutrients", {'query': search_phrase }, headers= headers)
+    resp = JSON.parse(resp)['foods'][0]
+    food = Food.find_or_create_by(
+      name: resp['food_name'],
+      serving_grams: resp['serving_weight_grams'],
+      calories: resp['nf_calories'],
+      fat: resp['nf_total_fat'],
+      saturated_fat: resp['nf_saturated_fat'],
+      carbs: resp['nf_total_carbohydrate'],
+      protein: resp['nf_protein'],
+      cholesterol: resp['nf_cholesterol'],
+      dietary_fiber: resp['nf_dietary_fiber'],
+      potassium: resp['nf_potassium'],
+      sodium: resp['nf_sodium'],
+      # serving_unit: resp['serving_unit'],
+      sugars: resp['nf_sugars']
+    )
+    
     # byebug
-    render json: resp
+    render json: food
+
+    # resp.foods[0].food_name nf_calories 
+    # _cholesterol _dietary_fiber _potassium 
+    # _protein _saturated_fat _sodium _sugars 
+    # _total_carbohydrate _total_fat serving_unit 
+    # serving_weight_grams
 
   end
 
