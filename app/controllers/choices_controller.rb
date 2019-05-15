20cf5b54022f8eb3c71856a5c70fee28e89020a0
James Dorr <jamesdorr3@gmail.com>
2019-05-15 18:31:09 -0400

    choices update on blur, kind of on exit

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -4,4 +4,17 @@ class ChoicesController < ApplicationController
     render :json => Choice.all
   end
 
+  def update
+    choice = Choice.find(params[:id])
+    choice.update(choice_params)
+    choice.save
+    # byebug
+  end
+
+  private
+
+  def choice_params
+    params.require(:choice).permit(:amount)
+  end
+
 end
