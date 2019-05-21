a391734f59e541c3d19456c3b5deac29dca1d63d
James Dorr <jamesdorr3@gmail.com>
2019-05-21 14:37:04 -0400

    update goal works

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,15 @@
 class GoalsController < ApplicationController
+
+  def update
+    goal = Goal.find(params[:id])
+    goal.update(goals_params)
+    render json: goal
+  end
+
+  private
+
+  def goals_params
+    params.require(:goal).permit(:id, :user_id, :calories, :fat, :carbs, :protein, :name)
+  end
+
 end
