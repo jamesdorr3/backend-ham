3261bc51ce8f875b6f2afe303bc967f8de62ac73
James Dorr <jamesdorr3@gmail.com>
2019-05-25 15:27:15 -0400

    finished save all controller, for now

--- app/controllers/save_all_controller.rb
+++ app/controllers/save_all_controller.rb
@@ -9,31 +9,21 @@ class SaveAllController < ApplicationController
       choice = Choice.find(choiceFood[:choice][:id])
       choice.update(choiceFood.require(:choice).permit(:food_id, :nix_id, :nix_name, :day_id, :amount, :measure, :category_id, :index))
     end
-    # byebug
-    # params[:days].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
-    # params[:day].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
-    # params[:goals].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
-    # params[:goal].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
-    # params[:user].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
-    # params[:user].each do |category| 
-    #   cat = Category.find(category.id)
-    #   cat.update(x.permit(:id, :name, :user_id, :index, :updated_at))
-    # end
+    params[:days].each do |day| 
+      d = Day.find(day[:id])
+      d.update(day.permit(:name, :goal_id))
+    end
+    day = Day.find(params[:day][:id])
+    day[:goal_id] = params[:goal][:id]
+    day[:name] = params[:day][:name]
+    day.save
+    params[:goals].each do |one_goal| 
+      goal = Goal.find(one_goal[:id])
+      goal.update(one_goal.permit(:calories, :fat, :carbs, :protein, :name))
+    end
+    goal = Goal.find(params[:goal][:id])
+    goal.update(params[:goal].permit(:calories, :fat, :carbs, :protein, :name))
+    # puts current_user[:email]
   end
 
   private
