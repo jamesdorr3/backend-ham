6b20b4af5ea5d8b66dd260d0ad0321e8d21f7b65
James Dorr <jamesdorr3@gmail.com>
2019-07-01 23:48:22 -0700

    please work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,12 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-  allow do
-    origins '*'
+  # allow do
+  #   origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
 
 end
