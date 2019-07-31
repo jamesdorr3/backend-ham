f5de62ac2107f6eeae1feac2603bf6febff6ccee
James Dorr <jamesdorr3@gmail.com>
2019-07-30 23:19:56 -0700

    more organized choice creation from USDA, ready to keep all

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -44,145 +44,28 @@ class SearchController < ApplicationController
   end
 
   def make_choice
-    category_id = params['categoryId']
-    search_id = params['fdcId']
-    key = Rails.application.credentials[:usda][:key]
-    resp = RestClient.get("https://#{key}@api.nal.usda.gov/fdc/v1/#{search_id}", headers= {'Content-Type':'application/json'})
-    resp = JSON.parse(resp)
 
-    if resp["householdServingFullText"]
-      unit_name = resp["householdServingFullText"]
-    elsif resp['foodPortions'][0] && resp['foodPortions'][0]['portionDescription'] ############## UNIT NAME
-      unit_name = "#{resp['foodPortions'][0]['portionDescription']}"
-      unit_name = 'unit' if unit_name == 'Quantity not specified'
-    else
-      unit_name = 'unit'
-    end
-    
+    food = Food.find_or_create_and_update(params['fdcId'])
+    measures = food.measures
+
+    day = (current_user ? current_user.days.last : Day.all.first) ############# nil ?
     # byebug
-    if resp['servingSize']
-      serving_grams = resp['servingSize']
-    elsif resp['foodPortions'][0]
-      serving_grams = resp['foodPortions'][0]['gramWeight']
-    end
-    serving_grams = 1 if !serving_grams || serving_grams < 1
-
-    if resp['labelNutrients'] && resp['labelNutrients']['fat']
-      fat = resp['labelNutrients']['fat']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Total lipid (fat)'} ### FAT
-      fat = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Total lipid (fat)'}['amount'] * serving_grams * 0.01
-    else
-      fat = 0
-    end
-
-    if resp['labelNutrients'] && resp['labelNutrients']['carbohydrates']
-      carbs = resp['labelNutrients']['carbohydrates']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Carbohydrate, by difference'}
-      carbs = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Carbohydrate, by difference'}['amount'] * serving_grams * 0.01
-    else
-      carbs = 0
-    end
-
-    if resp['labelNutrients'] && resp['labelNutrients']['protein']
-      protein = resp['labelNutrients']['protein']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Protein'}
-      protein = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Protein'}['amount'] * serving_grams * 0.01
-    else
-      protein = 0
-    end
-
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Energy'}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == 'Energy'}['amount'] * serving_grams / 100
-    else
-      calories = fat * 9 + carbs * 4 + protein * 4
-    end
-
-    # Food.find_or_create_resp(resp, current_user)
-
-    # byebug
-    food = Food.find_or_create_by(
-      user: current_user,
-      name: resp['description'],
-      serving_grams: serving_grams,
-      calories: calories,
-      fat: fat,
-      # saturated_fat: saturatedFat,
-      carbs: carbs,
-      protein: protein,
-      # cholesterol: cholesterol,
-      # dietary_fiber: fiber,
-      # potassium: potassium,
-      # sodium: sodium,
-      # serving_unit_name: unit_name,
-      # serving_unit_amount: 1,
-      brand: resp['brandOwner'],
-      # sugars: sugars,
-      # calcium: calcium,
-      # iron: iron,
-      # trans_fat: transFat,
-    )
-    if food.choice_count
-      food.choice_count += 1
-    else
-      food.choice_count = food.choices.count + 1
-    end
-    food.save
-    if resp['foodClass'] == 'Branded' # success
-      measures = [Measure.find_or_create_by(
-        food: food,
-        amount: 1,
-        grams: serving_grams,
-        name: unit_name,
-      )]
-    else
-      measures = []
-      resp['foodPortions'].each do |portion| # success
-        if portion['portionDescription']
-          name = portion['portionDescription']
-        elsif portion['modifier']
-          name = portion['modifier']
-        else
-          name = 'ATTENTION'
-        end
-        if portion['amount']
-          amount = portion['amount']
-        else
-          amount = 1
-        end
-        amount = 1 if !(amount > 0)
-        measure = Measure.find_or_create_by(
-          food: food,
-          amount: amount,
-          grams: portion['gramWeight'],
-          name: name
-        )
-        measures.push(measure)
-      end
-    end
-    measures.push(Measure.find_or_create_by(
-      food: food,
-      amount: 1,
-      grams: 1,
-      name: 'grams'
-    ))
-    day = (current_user ? current_user.days.last : Day.all.first)
-    if measures.first.amount
+    if measures.first && measures.first.amount
       amount = measures.first.amount
     else
       amount = 1
     end
+    puts food, measures
     choice = Choice.create(
       food: food,
-      category_id: category_id, 
-      day_id: params['dayId'], ############################# PROBLEMS
-      amount: measures.first.amount, 
+      category_id: params['categoryId'], 
+      day_id: params['dayId'], ############################# PROBLEMS ? 
+      amount: amount, 
       measure_id: measures.first.id, 
       index: Time::new.to_i)
     puts choice.errors.full_messages
     # to_render = {choice: choice, food: food}
-    render json: {choice: choice, food: food, resp: resp, measures: measures}
+    render json: {choice: choice, food: food, measures: measures}
   end
 
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   has_many :choices
-  belongs_to :user
+  # belongs_to :user
   has_many :measures
 
   def create_grams_measure
@@ -12,11 +12,35 @@ class Food < ApplicationRecord
     )
   end
 
-  def self.find_or_create_resp(resp, current_user)
-    if Food.find_by(fdcId: resp['fdcId'])
-      puts 'YAaaaaaaaaYY'
-      return
+  def increment_count
+    if self.choice_count
+      self.choice_count += 1
+    else
+      self.choice_count = self.choices.count + 1
     end
+    self.save
+  end
+
+  def decrement_count
+    if food.choice_count && food.choice_count > 0
+      food.choice_count += 1
+    else
+      food.choice_count = 0
+    end
+    food.save
+  end
+
+  def self.find_or_create_and_update(fdcId)
+
+    if food = Food.find_by(fdcId: fdcId)
+      food.increment_count
+      return food
+    end
+
+    key = Rails.application.credentials[:usda][:key]
+    resp = RestClient.get("https://#{key}@api.nal.usda.gov/fdc/v1/#{fdcId}", headers= {'Content-Type':'application/json'})
+    resp = JSON.parse(resp)
+    
     if resp["householdServingFullText"]
       unit_name = resp["householdServingFullText"]
     elsif resp['foodPortions'][0] && resp['foodPortions'][0]['portionDescription'] ############## UNIT NAME
@@ -67,7 +91,6 @@ class Food < ApplicationRecord
     end
 
     food = Food.find_or_create_by(
-      user: current_user,
       name: resp['description'],
       serving_grams: serving_grams,
       calories: calories,
@@ -77,79 +100,126 @@ class Food < ApplicationRecord
       brand: resp['brandOwner'],
     )
 
-    # saturated_fat: saturatedFat,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+    food.add_info(resp)
+
+    if !(food.measures.length > 0)
+      if resp['foodClass'] == 'Branded' # success
+        measures = [Measure.find_or_create_by(
+          food: food,
+          amount: 1,
+          grams: serving_grams,
+          name: unit_name,
+        )]
+      else
+        measures = []
+        resp['foodPortions'].each do |portion| # success
+          if portion['portionDescription']
+            name = portion['portionDescription']
+          elsif portion['modifier']
+            name = portion['modifier']
+          else
+            name = 'unit'
+          end
+          if portion['amount']
+            amount = portion['amount']
+          else
+            amount = 1
+          end
+          amount = 1 if !(amount > 0)
+          measure = Measure.create(
+            food: food,
+            amount: amount,
+            grams: portion['gramWeight'],
+            name: name
+          )
+          
+        end
+      end
+      Measure.find_or_create_by(
+        food: food,
+        amount: 1,
+        grams: 1,
+        name: 'grams'
+      )
+    end
+
+    return food
+
+  end
+
+  def add_info(resp)
+
+    if resp['labelNutrients'] && resp['labelNutrients']["saturatedFat"]
+      saturated_fat = resp['labelNutrients']["saturatedFat"]['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total saturated"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total saturated"}['amount'] * serving_grams / 100
+      saturated_fat = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total saturated"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      saturated_fat = nil
     end
     
-    # cholesterol: cholesterol,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+    if resp['labelNutrients'] && resp['labelNutrients']["cholesterol"]
+      cholesterol = resp['labelNutrients']["cholesterol"]['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Cholesterol"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Cholesterol"}['amount'] * serving_grams / 100
+      cholesterol = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Cholesterol"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      cholesterol = nil
     end
-    # dietary_fiber: fiber,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+
+    if resp['labelNutrients'] && resp['labelNutrients']['fiber']
+      fiber = resp['labelNutrients']['fiber']['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fiber, total dietary"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fiber, total dietary"}['amount'] * serving_grams / 100
+      fiber = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fiber, total dietary"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      fiber = nil
     end
-    # potassium: potassium,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+
+    if resp['labelNutrients'] && resp['labelNutrients']['potassium']
+      potassium = resp['labelNutrients']['potassium']['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Potassium, K"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Potassium, K"}['amount'] * serving_grams / 100
+      potassium = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Potassium, K"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      potassium = nil
     end
-    # sodium: sodium,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+
+    if resp['labelNutrients'] && resp['labelNutrients']['sodium']
+      sodium = resp['labelNutrients']['sodium']['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sodium, Na"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sodium, Na"}['amount'] * serving_grams / 100
+      sodium = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sodium, Na"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      sodium = nil
     end
-    # sugars: sugars,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+
+    if resp['labelNutrients'] && resp['labelNutrients']['sugars']
+      sugars = resp['labelNutrients']['sugars']['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sugars, total including NLEA"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sugars, total including NLEA"}['amount'] * serving_grams / 100
+      sugars = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Sugars, total including NLEA"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      sugars = nil
     end
-    # calcium: calcium,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
+
+    if resp['labelNutrients'] && resp['labelNutrients']['calcium']
+      calcium = resp['labelNutrients']['calcium']['value']
     elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Calcium, Ca"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Calcium, Ca"}['amount'] * serving_grams / 100
+      calcium = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Calcium, Ca"}['amount'] * serving_grams / 100
     else
-      calories = fat * 9 + carbs * 4 + protein * 4
+      calcium = nil
     end
-    # iron: iron,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Iron, Fe"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Iron, Fe"}['amount'] * serving_grams / 100
-    else
-      calories = fat * 9 + carbs * 4 + protein * 4
-    end
-    # trans_fat: transFat,
-    if resp['labelNutrients'] && resp['labelNutrients']['calories']
-      calories = resp['labelNutrients']['calories']['value']
-    elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total trans"}
-      calories = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total trans"}['amount'] * serving_grams / 100
-    else
-      calories = fat * 9 + carbs * 4 + protein * 4
-    end
+
+    # if resp['labelNutrients'] && resp['labelNutrients']['iron']
+    #   iron = resp['labelNutrients']['iron']['value']
+    # elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Iron, Fe"}
+    #   iron = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Iron, Fe"}['amount'] * serving_grams / 100
+    # else
+    #   iron = nil
+    # end
+
+    # if resp['labelNutrients'] && resp['labelNutrients']["transFat"]
+    #   trans_fat = resp['labelNutrients']["transFat"]['value']
+    # elsif resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total trans"}
+    #   trans_fat = resp["foodNutrients"].find{|x| x["nutrient"]['name'] == "Fatty acids, total trans"}['amount'] * serving_grams / 100
+    # else
+    #   trans_fat = nil
+    # end
 
     if resp['gtinUpc']
       upc = resp['gtinUpc']
@@ -157,66 +227,61 @@ class Food < ApplicationRecord
       upc = nil
     end
 
-    food.update(fdcId: resp['fdcId'])
+    self.update(fdcId: upc,cholesterol: cholesterol, dietary_fiber: fiber, potassium: potassium, saturated_fat: saturated_fat, sodium: sodium, sugars: sugars)
+    self.increment_count 
 
-    if food.choice_count
-      food.choice_count += 1
-    else
-      food.choice_count = food.choices.count + 1
-    end
-    food.save
-    if resp['foodClass'] == 'Branded' # success
-      measures = [Measure.find_or_create_by(
-        food: food,
-        amount: 1,
-        grams: serving_grams,
-        name: unit_name,
-      )]
-    else
-      measures = []
-      resp['foodPortions'].each do |portion| # success
-        if portion['portionDescription']
-          name = portion['portionDescription']
-        elsif portion['modifier']
-          name = portion['modifier']
-        else
-          name = 'ATTENTION'
-        end
-        if portion['amount']
-          amount = portion['amount']
-        else
-          amount = 1
-        end
-        amount = 1 if !(amount > 0)
-        measure = Measure.find_or_create_by(
-          food: food,
-          amount: amount,
-          grams: portion['gramWeight'],
-          name: name
-        )
-        measures.push(measure)
-      end
-    end
-    measures.push(Measure.find_or_create_by(
-      food: food,
-      amount: 1,
-      grams: 1,
-      name: 'grams'
-    ))
-    day = (current_user ? current_user.days.last : Day.all.first)
-    if measures.first.amount
-      amount = measures.first.amount
-    else
-      amount = 1
-    end
-    choice = Choice.create(
-      food: food,
-      category_id: category_id, 
-      day_id: params['dayId'], ############################# PROBLEMS
-      amount: measures.first.amount, 
-      measure_id: measures.first.id, 
-      index: Time::new.to_i)
-    puts choice.errors.full_messages
+    # if resp['foodClass'] == 'Branded' # success
+    #   measures = [Measure.find_or_create_by(
+    #     food: food,
+    #     amount: 1,
+    #     grams: serving_grams,
+    #     name: unit_name,
+    #   )]
+    # else
+    #   measures = []
+    #   resp['foodPortions'].each do |portion| # success
+    #     if portion['portionDescription']
+    #       name = portion['portionDescription']
+    #     elsif portion['modifier']
+    #       name = portion['modifier']
+    #     else
+    #       name = 'ATTENTION'
+    #     end
+    #     if portion['amount']
+    #       amount = portion['amount']
+    #     else
+    #       amount = 1
+    #     end
+    #     amount = 1 if !(amount > 0)
+    #     measure = Measure.find_or_create_by(
+    #       food: food,
+    #       amount: amount,
+    #       grams: portion['gramWeight'],
+    #       name: name
+    #     )
+    #     measures.push(measure)
+    #   end
+    # end
+    # measures.push(Measure.find_or_create_by(
+    #   food: food,
+    #   amount: 1,
+    #   grams: 1,
+    #   name: 'grams'
+    # ))
+    # day = (current_user ? current_user.days.last : Day.all.first)
+    # if measures.first.amount
+    #   amount = measures.first.amount
+    # else
+    #   amount = 1
+    # end
+    # choice = Choice.create(
+    #   food: food,
+    #   category_id: category_id, 
+    #   day_id: params['dayId'], ############################# PROBLEMS
+    #   amount: measures.first.amount, 
+    #   measure_id: measures.first.id, 
+    #   index: Time::new.to_i)
+    # puts choice.errors.full_messages
   end
 
 end
